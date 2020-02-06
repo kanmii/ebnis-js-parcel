f637ffa860a5158e3df226be520cc11d979c29b3
Kanmii <maneptha@gmail.com>
2020-02-06 12:39:44 +0100

    Making a realistic app that uses all installed packages.
    To be sure users will not have any surprises when using this
    template.

--- src/components/App/app-component.tsx
+++ src/components/App/app-component.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import SVG from "react-inlinesvg";
+import logo from "./logo.svg";
+import "./app.css";
 
 export function App() {
-  return <div>Parcel is awesome</div>;
+  return (
+    <>
+      <div className="plus-overlay" />
+
+      <header className="header">
+        <SVG src={logo} className="logo" />
+      </header>
+
+      <section className="main">
+        <div className="notification is-primary">
+          <button className="delete"></button>1
+        </div>
+
+        <button className="button">+</button>
+        <button className="button">-</button>
+      </section>
+    </>
+  );
 }
